5169c8e1e3a77f9d3feaabc8b774d8f4f8941c0f
Gregg Trunnell <gggtrunnell@gmail.com>
2024-10-24 00:31:28 -0500

    added 6 albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,21 +8,29 @@ function addToCollection(collection, title, artist, yearPublished){
         artist: artist,
         yearPublished: yearPublished,
       }
-collection.push(record);
-return record;
+   collection.push(record);
+    return record;
 }//end of addToCollection
 
-addToCollection(myCollection, 'OK Computer', 'Radiohead', 1997);
+let album1=addToCollection(myCollection, 'OK Computer', 'Radiohead', 1997);
+console.log(album1);
+
+let album2=addToCollection(myCollection, 'Lost Souls', 'Doves', 2000);
+console.log(album2);
+
+let album3=addToCollection(myCollection, 'Jubilee', 'Japanese Breakfast', 2021);
+console.log(album3);
+
+let album4=addToCollection(myCollection, 'Lift Your Skinny Fists Like Antennas to Heavan', 'Godspeed You! Black Emperor', 2000);
+console.log(album4);
+
+let album5=addToCollection(myCollection, 'A Moon Shaped Pool', 'Radiohead', 2016);
+console.log(album5);
+
+let album6=addToCollection(myCollection, 'The Gods We Can Touch', 'Aurora', 2022);
+console.log(album6);
+
 console.log(myCollection);
-
-addToCollection(myCollection, 'Lost Souls', 'Doves', 2000);
-
-
-
-
-
-
-
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
 // as a lil' chunk of friendly code that you don't need to understand right now.
